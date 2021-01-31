c2d5187f9946654a0e980a23e36111f5161aefcd
pgrho <shipwreck@live.jp>
2021-01-31 21:22:50 +0900

    Added SharedResourceDictionary

---
+++ src/Core/PresentationFramework/ViewModelUtils/SharedResourceDictionary.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+
+namespace Shipwreck.ViewModelUtils
+{
+    public sealed class SharedResourceDictionary : ResourceDictionary
+    {
+        public static readonly Dictionary<Uri, ResourceDictionary> _Instances = new Dictionary<Uri, ResourceDictionary>();
+
+        private Uri _Source;
+
+        public new Uri Source
+        {
+            get => _Source;
+            set
+            {
+                var old = _Source;
+                if (value != _Source)
+                {
+                    _Source = value;
+
+                    if (!_Instances.ContainsKey(value))
+                    {
+                        base.Source = value;
+
+                        _Instances.Add(value, this);
+                    }
+                    else
+                    {
+                        MergedDictionaries.Add(_Instances[value]);
+                    }
+                    if (old != null)
+                    {
+                        var od = MergedDictionaries.FirstOrDefault(e => e.Source == old);
+                        if (od != null)
+                        {
+                            MergedDictionaries.Remove(od);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
