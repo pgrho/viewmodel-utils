e4803845f99a36983e5625cca5b13c9b1c9b7daa
pgrho <shipwreck@live.jp>
2021-02-17 11:29:49 +0900

    Added ObservableCollectionTraceListener

---
+++ src/Core/Shared/ViewModelUtils/Diagnostics/TraceEventModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Shipwreck.ViewModelUtils.Diagnostics
+{
+    public sealed class TraceEventModel
+    {
+        internal TraceEventModel(
+            TraceEventCache eventCache,
+            string source,
+            TraceEventType eventType,
+            int id,
+            string message,
+            object[] data)
+        {
+            ProcessId = eventCache.ProcessId;
+            ThreadId = eventCache.ThreadId;
+            DateTime = eventCache.DateTime;
+
+            Source = source;
+
+            EventType = eventType;
+            Id = id;
+
+            Message = message != null ? (data?.Length > 0 ? string.Format(message, data) : message)
+                    : data != null ? string.Join(" ", data) : null;
+        }
+
+        public int ProcessId { get; }
+        public string ThreadId { get; }
+        public DateTime DateTime { get; }
+        public string Source { get; }
+
+        public TraceEventType EventType { get; }
+        public int Id { get; }
+
+        public string Message { get; }
+    }
+}
