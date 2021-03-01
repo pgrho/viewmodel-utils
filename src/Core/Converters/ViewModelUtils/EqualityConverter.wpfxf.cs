65b86199ac7c5e30bc1857885ad6456638afb892
pgrho <shipwreck@live.jp>
2021-03-02 08:04:00 +0900

    Added EqualityConverter

---
+++ src/Core/Converters/ViewModelUtils/EqualityConverter.wpfxf.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Globalization;
+
+namespace Shipwreck.ViewModelUtils
+{
+    public sealed partial class EqualityConverter : BooleanConverterBase
+    {
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+            => ToResult(Equals(value, parameter), targetType, culture);
+    }
+}
---
+++ src/Core/PresentationFramework/ViewModelUtils/EqualityConverter.wpf.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Shipwreck.ViewModelUtils
+{
+    public partial class EqualityConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            for (var i = 1; i < values.Length; i++)
+            {
+                if (!Equals(values[0], values[i]))
+                {
+                    return ToResult(false, targetType, culture);
+                }
+            }
+            return ToResult(true, targetType, culture);
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+            => throw new NotSupportedException();
+    }
+}
