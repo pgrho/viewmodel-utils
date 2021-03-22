0a80605aca9618a86e9b60519ba81f018dd81a97
pgrho <shipwreck@live.jp>
2021-03-22 11:49:24 +0900

    Fix PasswordBoxService (0.1.2-alpha2)

--- src/Core/PresentationFramework/ViewModelUtils/Controls/PasswordBoxService.cs
+++ src/Core/PresentationFramework/ViewModelUtils/Controls/PasswordBoxService.cs
@@ -5,10 +5,30 @@ namespace Shipwreck.ViewModelUtils.Controls
 {
     public static class PasswordBoxService
     {
+        public static readonly DependencyProperty HandlePasswordChangedProperty
+           = DependencyProperty.RegisterAttached("HandlePasswordChanged", typeof(bool?), typeof(PasswordBoxService), new FrameworkPropertyMetadata(null, (d, e) =>
+           {
+               if (d is PasswordBox pb)
+               {
+                   pb.RemoveHandler(PasswordBox.PasswordChangedEvent, (RoutedEventHandler)Pb_PasswordChanged);
+
+                   if (e.NewValue is bool b && b)
+                   {
+                       pb.AddHandler(PasswordBox.PasswordChangedEvent, (RoutedEventHandler)Pb_PasswordChanged);
+                   }
+               }
+           }));
+
         public static readonly DependencyProperty PasswordProperty
             = DependencyProperty.RegisterAttached(
                 "Password", typeof(string), typeof(PasswordBoxService), new FrameworkPropertyMetadata(null, OnPasswordChanged)
                 { BindsTwoWayByDefault = true });
+
+        public static bool? GetHandlePasswordChanged(PasswordBox obj)
+            => (bool?)obj.GetValue(HandlePasswordChangedProperty);
+
+        public static void SetHandlePasswordChanged(PasswordBox obj, bool? value)
+            => obj.SetValue(HandlePasswordChangedProperty, value);
 
         public static string GetPassword(PasswordBox obj)
             => (string)obj.GetValue(PasswordProperty);
@@ -20,8 +40,8 @@ namespace Shipwreck.ViewModelUtils.Controls
         {
             if (d is PasswordBox pb)
             {
-                pb.RemoveHandler(PasswordBox.PasswordChangedEvent, (RoutedEventHandler)Pb_PasswordChanged);
-                pb.AddHandler(PasswordBox.PasswordChangedEvent, (RoutedEventHandler)Pb_PasswordChanged);
+                SetHandlePasswordChanged(pb, GetHandlePasswordChanged(pb) ?? true);
+
                 if (e.NewValue is string s && s != pb.Password)
                 {
                     pb.Password = s;
