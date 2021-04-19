5e4cf9bab648b881c53df2dbcae2316521baff17
pgrho <shipwreck@live.jp>
2021-04-19 17:34:55 +0900

    Added ViewModelCommands

---
+++ src/Core/Converters/ViewModelUtils/ViewModelCommands.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace Shipwreck.ViewModelUtils
+{
+    public static class ViewModelCommands
+    {
+        private sealed class ToggleIsExpandedCommand : ICommand
+        {
+            event EventHandler ICommand.CanExecuteChanged
+            {
+                add { }
+                remove { }
+            }
+
+            bool ICommand.CanExecute(object parameter)
+                => (parameter as IExpandable)?.IsExpandable == true;
+
+            void ICommand.Execute(object parameter)
+            {
+                if (parameter is IExpandable e)
+                {
+                    e.IsExpanded = !e.IsExpanded && e.IsExpandable;
+                }
+            }
+        }
+
+        public static ICommand ToggleIsExpanded { get; } = new ToggleIsExpandedCommand();
+    }
+}
