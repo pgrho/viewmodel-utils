fa8c2bcc2865a3ea0189a6a6270497235c985bf6
pgrho <shipwreck@live.jp>
2021-04-19 17:36:17 +0900

    Added BindingProxy

---
+++ src/Core/PresentationFramework/ViewModelUtils/Controls/BindingProxy.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+
+namespace Shipwreck.ViewModelUtils.Controls
+{
+    public sealed class BindingProxy : Freezable
+    {
+        public static readonly DependencyProperty DataProperty
+            = DependencyProperty.Register(nameof(Data), typeof(object), typeof(BindingProxy), new FrameworkPropertyMetadata(null));
+
+        public object Data
+        {
+            get => GetValue(DataProperty);
+            set => SetValue(DataProperty, value);
+        }
+
+        public static readonly DependencyProperty DisplayNameProperty
+          = DependencyProperty.Register(nameof(DisplayName), typeof(string), typeof(BindingProxy), new PropertyMetadata(null));
+
+        public string DisplayName
+        {
+            get { return (string)GetValue(DisplayNameProperty); }
+            set { SetValue(DisplayNameProperty, value); }
+        }
+
+        protected override Freezable CreateInstanceCore()
+            => new BindingProxy();
+    }
+}
