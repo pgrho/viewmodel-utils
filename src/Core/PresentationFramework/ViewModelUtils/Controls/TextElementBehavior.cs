24168547ba14dc08da58452bc9ab9913a932f5a2
pgrho <shipwreck@live.jp>
2021-04-19 17:38:20 +0900

    Added TextElementBehavior

---
+++ src/Core/PresentationFramework/ViewModelUtils/Controls/TextElementBehavior.cs
@@ -0,0 +1,38 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+
+namespace Shipwreck.ViewModelUtils.Controls
+{
+    public static class TextElementBehavior
+    {
+        public static TextDecorationCollection GetTextDecorations(DependencyObject obj) => (TextDecorationCollection)obj.GetValue(TextDecorationsProperty);
+
+        public static void SetTextDecorations(DependencyObject obj, TextDecorationCollection value) => obj.SetValue(TextDecorationsProperty, value);
+
+        public static readonly DependencyProperty TextDecorationsProperty
+            = DependencyProperty.RegisterAttached("TextDecorations", typeof(TextDecorationCollection), typeof(TextElementBehavior), new PropertyMetadata(null, (d, e) =>
+            {
+                if (d is TextBlock textBlock)
+                {
+                    textBlock.TextDecorations = e.NewValue as TextDecorationCollection;
+                }
+                else if (d is TextBox textBox)
+                {
+                    textBox.TextDecorations = e.NewValue as TextDecorationCollection;
+                }
+                else if (d is Paragraph p)
+                {
+                    p.TextDecorations = e.NewValue as TextDecorationCollection;
+                }
+                else if (d is Inline i)
+                {
+                    i.TextDecorations = e.NewValue as TextDecorationCollection;
+                }
+                else if (d is AccessText a)
+                {
+                    a.TextDecorations = e.NewValue as TextDecorationCollection;
+                }
+            }));
+    }
+}
