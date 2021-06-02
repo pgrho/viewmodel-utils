5350a5e1cb63d82fdaefb7a4736183f5bef6cf96
pgrho <shipwreck@live.jp>
2021-06-02 11:32:58 +0900

    Fix ProcessingDisabled

--- src/Core/PresentationFramework/ViewModelUtils/FrameworkPageViewModel.wpf.cs
+++ src/Core/PresentationFramework/ViewModelUtils/FrameworkPageViewModel.wpf.cs
@@ -9,33 +9,20 @@ namespace Shipwreck.ViewModelUtils
     {
         public struct ProcessingDisabled : IDisposable
         {
-            private readonly Dispatcher _Dispatcher;
             private readonly DispatcherProcessingDisabled _Disabled;
 
-            public ProcessingDisabled(Dispatcher dispatcher, DispatcherProcessingDisabled disabled)
+            public ProcessingDisabled(DispatcherProcessingDisabled disabled)
             {
-                _Dispatcher = dispatcher;
                 _Disabled = disabled;
             }
 
             public void Dispose()
             {
-                if (_Dispatcher != null)
+                try
                 {
-                    try
-                    {
-                        if (_Dispatcher.Thread == Thread.CurrentThread)
-                        {
-                            _Disabled.Dispose();
-                        }
-                        else
-                        {
-                            var ds = _Disabled;
-                            _Dispatcher.Invoke((Action)(() => ds.Dispose()));
-                        }
-                    }
-                    catch { }
+                    _Disabled.Dispose();
                 }
+                catch { }
             }
         }
 
@@ -48,14 +35,7 @@ namespace Shipwreck.ViewModelUtils
                 {
                     if (dp.Thread == Thread.CurrentThread)
                     {
-                        return new ProcessingDisabled(dp, dp.DisableProcessing());
-                    }
-                    else
-                    {
-                        DispatcherProcessingDisabled ds = default;
-                        dp.Invoke((Action)(() => ds = dp.DisableProcessing()));
-
-                        return new ProcessingDisabled(dp, ds);
+                        return new ProcessingDisabled(dp.DisableProcessing());
                     }
                 }
             }
