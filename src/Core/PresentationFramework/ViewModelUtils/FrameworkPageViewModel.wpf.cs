7b05b368474524243df847eca2f27f85d6f396ee
pgrho <shipwreck@live.jp>
2021-06-02 11:08:07 +0900

    Fix ProcessingDisabled (0.2.1-alpha0)

--- src/Core/PresentationFramework/ViewModelUtils/FrameworkPageViewModel.wpf.cs
+++ src/Core/PresentationFramework/ViewModelUtils/FrameworkPageViewModel.wpf.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 using System.Windows;
 using System.Windows.Threading;
 
@@ -8,19 +9,59 @@ namespace Shipwreck.ViewModelUtils
     {
         public struct ProcessingDisabled : IDisposable
         {
+            private readonly Dispatcher _Dispatcher;
             private readonly DispatcherProcessingDisabled _Disabled;
 
-            public ProcessingDisabled(DispatcherProcessingDisabled disabled)
+            public ProcessingDisabled(Dispatcher dispatcher, DispatcherProcessingDisabled disabled)
             {
+                _Dispatcher = dispatcher;
                 _Disabled = disabled;
             }
 
             public void Dispose()
-                => _Disabled.Dispose();
+            {
+                if (_Dispatcher != null)
+                {
+                    try
+                    {
+                        if (_Dispatcher.Thread == Thread.CurrentThread)
+                        {
+                            _Disabled.Dispose();
+                        }
+                        else
+                        {
+                            var ds = _Disabled;
+                            _Dispatcher.Invoke((Action)(() => ds.Dispose()));
+                        }
+                    }
+                    catch { }
+                }
+            }
         }
 
         public ProcessingDisabled DisableProcessing()
-            => new ProcessingDisabled(Application.Current?.Dispatcher.DisableProcessing() ?? default);
+        {
+            try
+            {
+                var dp = Application.Current?.Dispatcher;
+                if (dp != null)
+                {
+                    if (dp.Thread == Thread.CurrentThread)
+                    {
+                        return new ProcessingDisabled(dp, dp.DisableProcessing());
+                    }
+                    else
+                    {
+                        DispatcherProcessingDisabled ds = default;
+                        dp.Invoke((Action)(() => ds = dp.DisableProcessing()));
+
+                        return new ProcessingDisabled(dp, ds);
+                    }
+                }
+            }
+            catch { }
+            return default;
+        }
 
         static partial void PlatformGetInteractionService(ref IInteractionService service)
             => service = new InteractionService();
