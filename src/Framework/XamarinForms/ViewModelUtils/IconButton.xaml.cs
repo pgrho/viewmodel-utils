2801b8eee247cef1ed0dbcefcc7b60d3d3b7baf8
pgrho <shipwreck@live.jp>
2021-08-04 13:53:55 +0900

    Added IconButton

---
+++ src/Framework/XamarinForms/ViewModelUtils/IconButton.xaml.cs
@@ -0,0 +1,48 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Shipwreck.ViewModelUtils
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class IconButton : ContentView
+    {
+        public static readonly BindableProperty FontSizeProperty
+            = BindableProperty.Create(
+                nameof(FontSize), typeof(double), typeof(IconButton),
+                defaultValue: new FontSizeConverter().ConvertFromInvariantString("Large"));
+
+        public static readonly BindableProperty ButtonPaddingProperty
+            = BindableProperty.Create(
+                nameof(ButtonPadding), typeof(Thickness), typeof(IconButton),
+                defaultValue: new Thickness(16, 0));
+
+        public static readonly BindableProperty HasBorderProperty
+            = BindableProperty.Create(nameof(HasBorder), typeof(bool), typeof(IconButton), defaultValue: true);
+
+        public IconButton()
+        {
+            InitializeComponent();
+            Padding = new Thickness(0);
+        }
+
+        [TypeConverter(typeof(FontSizeConverter))]
+        public double FontSize
+        {
+            get => (double)GetValue(FontSizeProperty);
+            set => SetValue(FontSizeProperty, value);
+        }
+
+        [TypeConverter(typeof(ThicknessTypeConverter))]
+        public Thickness ButtonPadding
+        {
+            get => (Thickness)GetValue(ButtonPaddingProperty);
+            set => SetValue(ButtonPaddingProperty, value);
+        }
+
+        public bool HasBorder
+        {
+            get => (bool)GetValue(HasBorderProperty);
+            set => SetValue(HasBorderProperty, value);
+        }
+    }
+}
