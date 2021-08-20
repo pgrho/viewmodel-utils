986e2eb70d0955338a79809419a474668224659b
pgrho <shipwreck@live.jp>
2021-08-20 16:10:26 +0900

    Added MenuItemViewModel

---
+++ src/Core/Shared/ViewModelUtils/MenuPageGroupViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System.Linq;
+
+namespace Shipwreck.ViewModelUtils
+{
+    public partial class MenuPageGroupViewModel : ObservableModel
+    {
+        internal MenuPageGroupViewModel(string title)
+        {
+            Title = title;
+            Items = new BulkUpdateableCollection<MenuPageItemViewModel>();
+        }
+
+        public string Title { get; }
+
+        public BulkUpdateableCollection<MenuPageItemViewModel> Items { get; }
+
+        #region IsVisible
+
+        private bool _IsVisible;
+
+        public bool IsVisible
+        {
+            get => _IsVisible;
+            private set => SetProperty(ref _IsVisible, value);
+        }
+
+        #endregion IsVisible
+
+        public MenuPageItemViewModel AddAction(CommandViewModelBase command, bool isExtension = false)
+        {
+            var item = new MenuPageItemViewModel(this, command, isExtension: isExtension);
+            Items.Add(item);
+            Invalidate();
+            return item;
+        }
+
+        public MenuPageGroupViewModel Invalidate()
+        {
+            IsVisible = Items.Any(e => e.Command.IsVisible);
+            return this;
+        }
+    }
+}
---
+++ src/Core/Shared/ViewModelUtils/MenuPageItemViewModel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shipwreck.ViewModelUtils
+{
+    public sealed class MenuPageItemViewModel : ObservableModel
+    {
+        public MenuPageItemViewModel(MenuPageGroupViewModel group, CommandViewModelBase command, bool isExtension = false)
+        {
+            Group = group;
+            Command = command;
+            SubCommands = new BulkUpdateableCollection<CommandViewModelBase>();
+            IsExtension = isExtension;
+
+            Command.PropertyChanged += Command_PropertyChanged;
+        }
+
+        public MenuPageGroupViewModel Group { get; }
+
+        public CommandViewModelBase Command { get; }
+
+        public bool IsExtension { get; }
+
+        public BulkUpdateableCollection<CommandViewModelBase> SubCommands { get; }
+
+        public MenuPageItemViewModel AddAction(CommandViewModelBase command)
+            => Group.AddAction(command);
+
+        public MenuPageItemViewModel AddSubAction(CommandViewModelBase command)
+        {
+            SubCommands.Add(command);
+
+            return this;
+        }
+
+        private void Command_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName == nameof(Command.IsVisible))
+            {
+                Group.Invalidate();
+            }
+        }
+    }
+}
