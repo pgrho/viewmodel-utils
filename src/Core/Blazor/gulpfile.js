23629c91dcc0a92306e3b0d850411920ce4078d4
pgrho <shipwreck@live.jp>
2021-08-26 07:07:47 +0900

    Add bundled static web assets

--- src/Core/Blazor/gulpfile.js
+++ src/Core/Blazor/gulpfile.js
@@ -6,24 +6,28 @@ var del = require('del');
 var ts = require('gulp-typescript');
 
 gulp.task('clean', function () {
-    return del(['wwwroot/*.js']);
+    return del(['wwwroot/*.js', 'Scripts/Shipwreck.ViewModelUtils.Core.Blazor.js']);
 });
 gulp.task('tsc', function () {
     return gulp.src(['Scripts/*.ts']).pipe(ts({
-        outFile: 'Shipwreck.ViewModelUtils.Core.js'
-    })).pipe(gulp.dest('wwwroot/'));
+        outFile: 'Shipwreck.ViewModelUtils.Core.Blazor.js'
+    })).pipe(gulp.dest('Scripts/'));
 });
-gulp.task('minify', function () {
+gulp.task('concatjs', function () {
+    return gulp.src(['Scripts/Copyright.js', 'Scripts/Shipwreck.ViewModelUtils.Core.Blazor.js'])
+        .pipe(concat('Shipwreck.ViewModelUtils.Core.Blazor.js'))
+        .pipe(gulp.dest('wwwroot/'));
+});
+gulp.task('minifyjs', function () {
     return gulp.src([
-        'Scripts/Copyright.js',
-        'wwwroot/Shipwreck.ViewModelUtils.Core.js'
+        'wwwroot/Shipwreck.ViewModelUtils.Core.Blazor.js'
     ])
-        .pipe(concat('Shipwreck.ViewModelUtils.Core.min.js'))
         .pipe(uglify({
             output: {
                 comments: /^!/
             }
         }))
+        .pipe(concat('Shipwreck.ViewModelUtils.Core.Blazor.min.js'))
         .pipe(gulp.dest('wwwroot/'));
 });
-gulp.task('default', gulp.series(['clean', 'tsc', 'minify']));
+gulp.task('default', gulp.series(['clean', 'tsc', 'concatjs', 'minifyjs']));
--- src/Framework/Blazor/gulpfile.js
+++ src/Framework/Blazor/gulpfile.js
@@ -1,29 +1,60 @@
-﻿/// <binding BeforeBuild='default' Clean='clean' />
-var gulp = require("gulp");
+﻿var gulp = require("gulp");
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var del = require('del');
 var ts = require('gulp-typescript');
 
+var jtToast = process.env['PkgBlazorJqueryToast'];
+var typeahead = process.env['PkgBlazorTypeahead'];
+
 gulp.task('clean', function () {
-    return del(['wwwroot/*.js']);
+    return del(['wwwroot/*.js', 'Scripts/Shipwreck.ViewModelUtils.Blazor.js']);
 });
 gulp.task('tsc', function () {
     return gulp.src(['Scripts/*.ts']).pipe(ts({
-        outFile: 'all.js'
+        outFile: 'Shipwreck.ViewModelUtils.Blazor.js'
     })).pipe(gulp.dest('Scripts/'));
 });
-gulp.task('scripts', function () {
-    return gulp.src([
-        'Scripts/Copyright.js',
-        'Scripts/all.js'
-    ])
+gulp.task('concatjs', function () {
+    return gulp.src(['Scripts/Copyright.js', 'Scripts/Shipwreck.ViewModelUtils.Blazor.js'])
         .pipe(concat('Shipwreck.ViewModelUtils.Blazor.js'))
+        .pipe(gulp.dest('wwwroot/'));
+});
+gulp.task('minifyjs', function () {
+    return gulp.src([
+        'wwwroot/Shipwreck.ViewModelUtils.Blazor.js'
+    ])
         .pipe(uglify({
             output: {
                 comments: /^!/
             }
         }))
+        .pipe(concat('Shipwreck.ViewModelUtils.Blazor.min.js'))
         .pipe(gulp.dest('wwwroot/'));
 });
-gulp.task('default', gulp.series(['clean', 'tsc', 'scripts']));
+gulp.task('bundlejs', function () {
+    return gulp.src([
+        'node_modules/jquery/dist/jquery.min.js',
+        'node_modules/popper.js/dist/umd/popper-utils.min.js',
+        'node_modules/popper.js/dist/umd/popper.min.js',
+        'node_modules/bootstrap/dist/js/bootstrap.min.js',
+        'node_modules/moment/min/moment-with-locales.min.js',
+        'node_modules/tempusdominus-bootstrap-4/build/js/tempusdominus-bootstrap-4.min.js',
+        jtToast + '\\staticwebassets\\Shipwreck.BlazorJqueryToast.js',
+        typeahead + '\\staticwebassets\\Shipwreck.BlazorTypeahead.js',
+        '../../Core/Blazor/wwwroot/Shipwreck.ViewModelUtils.Core.Blazor.min.js',
+        'wwwroot/Shipwreck.ViewModelUtils.Blazor.min.js'
+    ])
+        .pipe(concat('Shipwreck.ViewModelUtils.Blazor.bundle.js'))
+        .pipe(gulp.dest('wwwroot/'));
+});
+gulp.task('bundlecss', function () {
+    return gulp.src([
+        'node_modules/bootstrap/dist/css/bootstrap.min.css',
+        'node_modules/tempusdominus-bootstrap-4/build/css/tempusdominus-bootstrap-4.min.css',
+        jtToast + '\\staticwebassets\\Shipwreck.BlazorJqueryToast.css' 
+    ])
+        .pipe(concat('Shipwreck.ViewModelUtils.Blazor.bundle.css'))
+        .pipe(gulp.dest('wwwroot/'));
+});
+gulp.task('default', gulp.series(['clean', 'tsc', 'concatjs', 'minifyjs', 'bundlejs']));
