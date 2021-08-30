de89da4f3c8eaebc4952fa537de1d8ea9858a9f1
pgrho <shipwreck@live.jp>
2021-08-30 23:31:59 +0900

    Added IFrameworkSearchModalViewModel (0.2.3-alpha001)

---
+++ src/Core/Shared/ViewModelUtils/Searching/IFrameworkSearchModalViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Shipwreck.ViewModelUtils.Searching
+{
+    public interface IFrameworkSearchModalViewModel : IFrameworkModalViewModel
+    {
+        IFrameworkSearchPageViewModel SearchPage { get; }
+
+        void Select(object item);
+    }
+}
