636ef271cb2a765009185a959ae0285fb14b62bd
pgrho <shipwreck@live.jp>
2021-08-30 15:47:07 +0900

    Added FormValidationSummary

---
+++ src/Framework/Blazor/Components/FormGroupTheme.cs
@@ -0,0 +1,12 @@
+﻿namespace Shipwreck.ViewModelUtils.Components
+{
+    public class FormGroupTheme
+    {
+        public static readonly FormGroupTheme Default = new FormGroupTheme();
+        public string FormGroupClass { get; set; } = "form-group";
+        public string LabelClass { get; set; } = "form-label";
+        public string ControlClass { get; set; } = "form-control";
+        public string SelectControlClass { get; set; } = "form-control custom-select";
+        public string ErrorMessageClass { get; set; } = "field-validation-valid text-danger";
+    }
+}
