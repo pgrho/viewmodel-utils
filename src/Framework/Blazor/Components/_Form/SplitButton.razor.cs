9a121d106405c14534b6e491bc95ced64030d891
pgrho <shipwreck@live.jp>
2021-08-30 16:05:58 +0900

    Added SplitButton

---
+++ src/Framework/Blazor/Components/_Form/SplitButton.razor.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Shipwreck.ViewModelUtils.Components
+{
+    public partial class SplitButton : ListComponentBase<ICommandViewModel, ICommandViewModel>
+    {
+        [Parameter]
+        public string BaseClass { get; set; }
+
+        [Parameter]
+        public bool? IsVisible { get; set; }
+
+        [Parameter]
+        public bool? IsEnabled { get; set; }
+
+        [Parameter]
+        public bool? IsActive { get; set; }
+
+        [Parameter]
+        public bool ShowIcon { get; set; } = true;
+
+        [Parameter]
+        public bool ShowTitle { get; set; } = true;
+
+        [Parameter]
+        public bool ShowBadge { get; set; } = true;
+    }
+}
