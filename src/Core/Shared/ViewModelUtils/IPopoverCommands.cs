4db48ba1c77f774c7ad3ef9887b13fc3a1a80fb0
pgrho <shipwreck@live.jp>
2021-09-08 14:10:20 +0900

    Added popover classes

---
+++ src/Core/Shared/ViewModelUtils/IPopoverCommands.cs
@@ -0,0 +1,9 @@
+﻿namespace Shipwreck.ViewModelUtils
+{
+    public interface IPopoverCommands
+    {
+        CommandViewModelCollection PrimaryCommands { get; }
+        CommandViewModelCollection StateCommands { get; }
+        CommandViewModelCollection PrintCommands { get; }
+    }
+}
---
+++ src/Framework/Blazor/Components/_Popover/PopoverAnchorCell.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Input;
+using Microsoft.AspNetCore.Components;
+
+namespace Shipwreck.ViewModelUtils.Components
+{
+    public abstract class PopoverAnchorCell<T> : BindableComponentBase<T>
+        where T : class
+    {
+        [Parameter]
+        public RenderFragment ChildContent { get; set; }
+
+        [Parameter]
+        public bool IsPrimary { get; set; } = true;
+
+        [Parameter]
+        [Obsolete]
+        public bool IsDark
+        {
+            get => !IsPrimary;
+            set => IsPrimary = !value;
+        }
+
+        [Parameter]
+        public ICommand Command { get; set; }
+
+        [Parameter]
+        public PopoverTargetCommandMode CommandMode { get; set; } = PopoverTargetCommandMode.Replace;
+
+        [Parameter(CaptureUnmatchedValues = true)]
+        public IDictionary<string, object> AdditionalAttributes { get; set; }
+    }
+}
