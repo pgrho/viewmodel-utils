ae39931148f5047f931b6da02973fdfebaa73ac1
pgrho <shipwreck@live.jp>
2022-06-03 11:47:21 +0900

    Added Interfaces to FrameworkLayoutBase

--- src/Core/Blazor/ViewModelUtils/Components/FrameworkLayoutBase.cs
+++ src/Core/Blazor/ViewModelUtils/Components/FrameworkLayoutBase.cs
@@ -1,7 +1,49 @@
 ﻿namespace Shipwreck.ViewModelUtils.Components;
 
-public abstract class FrameworkLayoutBase : BindableLayoutComponentBase<FrameworkLayoutViewModel>, IHasJSRuntime
+public abstract class FrameworkLayoutBase : BindableLayoutComponentBase<FrameworkLayoutViewModel>, IHasJSRuntime, IHasInteractionService, IHasModalPresenter, IHasPopoverPresenter
 {
     [Inject]
     public IJSRuntime JS { get; set; }
+
+    [Inject]
+    public IInteractionService Interaction { get; set; }
+
+
+    #region Page
+
+    private FrameworkPageBase _Page;
+
+    public FrameworkPageBase Page
+    {
+        get => _Page;
+        internal set
+        {
+            SetProperty(ref _Page, value);
+            OnPageChanged();
+        }
+    }
+
+    protected virtual void OnPageChanged()
+    {
+        PageContext = _Page?.DataContext;
+        ModalPresenter?.CloseModal();
+        PopoverPresenter?.CloseModal();
+    }
+
+    #endregion Page
+
+    #region PageContext
+
+    private FrameworkPageViewModel _PageContext;
+
+    public FrameworkPageViewModel PageContext
+    {
+        get => _PageContext;
+        internal set => SetProperty(ref _PageContext, value);
+    }
+
+    #endregion PageContext
+    public abstract ModalPresenterBase ModalPresenter { get; }
+
+    public abstract ModalPresenterBase PopoverPresenter { get; }
 }
--- src/Demo/XamarinForms.Android/Resources/Resource.designer.cs
+++ src/Demo/XamarinForms.Android/Resources/Resource.designer.cs
@@ -14,7 +14,7 @@ namespace Shipwreck.ViewModelUtils.Demo.XamarinForms.Droid
 {
 	
 	
-	[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Android.Build.Tasks", "12.2.0.153")]
+	[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Android.Build.Tasks", "12.3.99.43")]
 	public partial class Resource
 	{
 		
