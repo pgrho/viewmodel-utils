e6fd9f95ed8395dfd2de11516e509b9cf43e9951
pgrho <shipwreck@live.jp>
2023-03-31 15:40:37 +0900

    Added EventModelPageLogger

---
+++ src/Core/PresentationFramework/ViewModelUtils/EventModelPageLogger.cs
@@ -0,0 +1,35 @@
+﻿namespace Shipwreck.ViewModelUtils;
+
+public class EventModelPageLogger : IPageLogger
+{
+    private static readonly int ProcessId = Process.GetCurrentProcess()?.Id ?? 0;
+
+    public EventModelPageLogger(string name)
+    {
+        Name = name;
+    }
+
+    public string Name { get; }
+
+    public void Log(TraceEventType eventType, int eventId, string message)
+        => EventModel.Add(
+            new EventModel(
+                ProcessId,
+                Thread.CurrentThread?.ManagedThreadId.ToString(),
+                DateTime.Now,
+                Name,
+                eventType,
+                eventId,
+                message));
+
+    public void Log(TraceEventType eventType, int eventId, string format, object[] args)
+        => EventModel.Add(
+            new EventModel(
+                ProcessId,
+                Thread.CurrentThread?.ManagedThreadId.ToString(),
+                DateTime.Now,
+                Name,
+                eventType,
+                eventId,
+                string.Format(format, args)));
+}
