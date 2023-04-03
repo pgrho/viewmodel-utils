e516179d3a9e1d0639e497b8d36d85dd1b3b63f7
pgrho <shipwreck@live.jp>
2023-04-03 10:17:07 +0900

    Split Blazor demo projects

---
+++ src/Demo/Blazor.Wpf/MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Net.Http;
+using System.Windows;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Shipwreck.ViewModelUtils.Demo.Blazor
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var serviceCollection = new ServiceCollection();
+            serviceCollection.AddWpfBlazorWebView();
+            serviceCollection.AddScoped(sp => new HttpClient { BaseAddress = new Uri("/") });
+            serviceCollection.AddScoped(sp => new InteractionService());
+            var sp = serviceCollection.BuildServiceProvider();
+
+            Resources.Add("services", sp);
+        }
+    }
+}
