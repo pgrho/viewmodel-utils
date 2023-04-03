6061e3e49aed79787bcf21ccb5ad9e5a3dee4111
pgrho <shipwreck@live.jp>
2023-04-03 11:00:31 +0900

    Fix static web assets of Demo.Blazor.Wpf

--- src/Demo/Blazor.Wpf/MainWindow.xaml.cs
+++ src/Demo/Blazor.Wpf/MainWindow.xaml.cs
@@ -15,11 +15,27 @@ namespace Shipwreck.ViewModelUtils.Demo.Blazor
 
             var serviceCollection = new ServiceCollection();
             serviceCollection.AddWpfBlazorWebView();
+            serviceCollection.AddBlazorWebViewDeveloperTools(); 
             serviceCollection.AddScoped(sp => new HttpClient { BaseAddress = new Uri("/") });
             serviceCollection.AddScoped(sp => new InteractionService());
             var sp = serviceCollection.BuildServiceProvider();
 
             Resources.Add("services", sp);
+        }
+
+        private async void webView_Loaded(object sender, RoutedEventArgs e)
+        {
+            await webView.WebView.EnsureCoreWebView2Async();
+
+            var cwv = webView.WebView.CoreWebView2;
+
+            cwv.WebResourceResponseReceived += (s, e) =>
+            {
+                if (e.Request.Uri?.StartsWith("data:") == false)
+                {
+                    Debug.WriteLine($"{e.Request.Method} {e.Request.Uri}: {e.Response.StatusCode}");
+                }
+            };
         }
     }
 }
