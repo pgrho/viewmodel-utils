98ee9387c357632504d5d969011fbdc04f57346b
pgrho <shipwreck@live.jp>
2023-07-31 14:51:03 +0900

    Added SearchPropertyGroupViewModel.IsExpanded

--- src/Core/Shared/ViewModelUtils/Searching/SearchPropertyGroupViewModel.cs
+++ src/Core/Shared/ViewModelUtils/Searching/SearchPropertyGroupViewModel.cs
@@ -6,6 +6,7 @@ public sealed class SearchPropertyGroupViewModel : ObservableModel
     {
         Modal = modal;
         AncestorPath = ancestorPath;
+        _IsExpanded = string.IsNullOrEmpty(ancestorPath);
         Items = Array.AsReadOnly(properties.ToArray());
     }
 
@@ -15,6 +16,21 @@ public sealed class SearchPropertyGroupViewModel : ObservableModel
         : (Modal.SearchPage.Properties.FirstOrDefault(e => e.Name == AncestorPath)?.DisplayName ?? AncestorPath);
 
     public string AncestorPath { get; }
+
+    #region IsExpanded
+
+    private bool _IsExpanded;
+
+    public bool IsExpanded
+    {
+        get => _IsExpanded;
+        set => SetProperty(ref _IsExpanded, value);
+    }
+
+    public void ToggleIsExpanded()
+        => IsExpanded = !IsExpanded;
+
+    #endregion IsExpanded
 
     public ReadOnlyCollection<SearchPropertyViewModel> Items { get; }
 }
