307738f5507975b3711952922c613a4c8d0a9c3f
pgrho <shipwreck@live.jp>
2023-11-24 14:46:52 +0900

    Make SearchPropertyGroupViewModel.DisplayName configurable

--- src/Core/Shared/ViewModelUtils/Searching/SearchPropertyGroupViewModel.cs
+++ src/Core/Shared/ViewModelUtils/Searching/SearchPropertyGroupViewModel.cs
@@ -2,18 +2,29 @@
 
 public class SearchPropertyGroupViewModel : ObservableModel
 {
-    protected internal SearchPropertyGroupViewModel(SearchPropertiesModalViewModel modal, string ancestorPath, IEnumerable<SearchPropertyViewModel> properties)
+    protected internal SearchPropertyGroupViewModel(SearchPropertiesModalViewModel modal, string ancestorPath, IEnumerable<SearchPropertyViewModel> properties, string displayName = null)
     {
         Modal = modal;
         AncestorPath = ancestorPath;
         _IsExpanded = string.IsNullOrEmpty(ancestorPath);
         Items = Array.AsReadOnly(properties.ToArray());
+        _DisplayName = displayName;
     }
 
     SearchPropertiesModalViewModel Modal { get; }
+
+    #region DisplayName
+
+    private string _DisplayName;
+
     public string DisplayName
-        => AncestorPath == null ? null
+        => (_DisplayName ??= GetDisplayName()).TrimOrNull();
+
+    protected virtual string GetDisplayName()
+        => AncestorPath == null ? string.Empty
         : (Modal.SearchPage.Properties.FirstOrDefault(e => e.Name == AncestorPath)?.DisplayName ?? AncestorPath);
+
+    #endregion DisplayName
 
     public string AncestorPath { get; }
 
