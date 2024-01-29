441d0da8d397813d041d827fd24c69f13d287a94
pgrho <shipwreck@live.jp>
2024-01-29 10:44:07 +0900

    Add ArithmeticConverter

---
+++ src/Core/PresentationFramework/ViewModelUtils/ArithmeticConverter.cs
@@ -0,0 +1,16 @@
+﻿namespace Shipwreck.ViewModelUtils;
+
+[ValueConversion(typeof(double), typeof(double))]
+public abstract class ArithmeticConverter : IValueConverter
+{
+    public abstract double Operand { get; set; }
+
+    // TODO other numeric targetType
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        => Calculate((value as IConvertible)?.ToDouble(culture) ?? double.NaN, Operand);
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        => throw new NotSupportedException();
+
+    protected abstract double Calculate(double x, double y);
+}
---
+++ src/Core/PresentationFramework/ViewModelUtils/MultiplyConverter.cs
@@ -0,0 +1,10 @@
+﻿namespace Shipwreck.ViewModelUtils;
+
+public sealed class MultiplyConverter : ArithmeticConverter
+{
+    [DefaultValue(1.0)]
+    public override double Operand { get; set; } = 1;
+
+    protected override double Calculate(double x, double y)
+        => x * y;
+}
