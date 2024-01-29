7966e8d2ac612cf6319f5eeb26f68a2e8296f0de
pgrho <shipwreck@live.jp>
2024-01-29 10:38:55 +0900

    Add PageCommandBuilder

--- src/Core/Shared/ViewModelUtils/_Commands/FrameworkPageCommandBuilder.cs
+++ src/Core/Shared/ViewModelUtils/_Commands/FrameworkPageCommandBuilder.cs
@@ -1,5 +1,64 @@
 ﻿namespace Shipwreck.ViewModelUtils;
 
+public abstract class PageCommandBuilder : CommandBuilderBase
+{
+    protected PageCommandBuilder(FrameworkPageViewModel page)
+    {
+        Page = page;
+    }
+
+    protected FrameworkPageViewModel Page { get; }
+
+    protected virtual void OnExecuted()
+    {
+        var ne = GetNavigationEntry();
+        if (ne != null)
+        {
+            Page?.NavigateTo(ne);
+        }
+    }
+
+    protected abstract NavigationEntry GetNavigationEntry();
+
+    protected string ComputeHref(CommandViewModelBase command)
+        => HrefGetter?.Invoke(command) ?? Href ?? GetNavigationEntry()?.Path;
+
+    protected virtual string ComputeIcon(CommandViewModelBase command)
+        => IconGetter?.Invoke(command) ?? Icon;
+
+    protected virtual string ComputeTitle(CommandViewModelBase command)
+        => TitleGetter?.Invoke(command) ?? Title;
+
+    protected virtual string ComputeDescription(CommandViewModelBase command)
+        => DescriptionGetter?.Invoke(command) ?? Description;
+
+    protected virtual bool ComputeIsVisible(CommandViewModelBase command)
+        => IsVisibleGetter?.Invoke(command) ?? IsVisible ?? IsSupported();
+
+    protected virtual bool ComputeIsEnabled(CommandViewModelBase command)
+        => IsSupported() && (IsEnabledGetter?.Invoke(command) ?? IsEnabled ?? true);
+
+    protected bool IsSupported()
+        => GetNavigationEntry() is var e
+        && e?.Path != null
+        && Page?.Navigation is var n
+        && n.IsSupported(Page, e);
+
+    public override CommandViewModelBase Build()
+        => CommandViewModel.Create(
+            _ => OnExecuted(),
+            titleGetter: ComputeTitle,
+            descriptionGetter: ComputeDescription,
+            isVisibleGetter: ComputeIsVisible,
+            isEnabledGetter: ComputeIsEnabled,
+            iconGetter: ComputeIcon,
+            style: Style ?? default,
+            styleGetter: StyleGetter,
+            hrefGetter: ComputeHref,
+            badgeCount: BadgeCount ?? 0,
+            badgeCountGetter: BadgeCountGetter);
+}
+
 public abstract class FrameworkPageCommandBuilder : CommandBuilderBase
 {
     protected FrameworkPageCommandBuilder(FrameworkPageViewModel page)
