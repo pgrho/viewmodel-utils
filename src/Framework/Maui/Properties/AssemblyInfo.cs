0bce177a0d8466f6c736353871bc4692bbb4fc63
pgrho <shipwreck@live.jp>
2024-03-06 10:10:15 +0900

    Add MAUI IconButton

---
+++ src/Framework/Maui/Properties/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+﻿global using System.Windows.Input;
+global using Microsoft.Maui;
+global using Microsoft.Maui.Controls;
+global using Microsoft.Maui.Graphics;
+
+[assembly: XmlnsDefinition(XMLNS.URL, "Shipwreck.ViewModelUtils")]
+[assembly: Microsoft.Maui.Controls.XmlnsPrefix(XMLNS.URL, XMLNS.PREFIX)]
