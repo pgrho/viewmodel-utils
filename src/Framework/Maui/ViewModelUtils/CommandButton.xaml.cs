c55e6d1f1da7b8ff66e8b70f51085c8bf0a237a0
pgrho <shipwreck@live.jp>
2024-03-06 10:40:04 +0900

    MAUI CommandButtonを追加

---
+++ src/Framework/Maui/ViewModelUtils/CommandButton.xaml.cs
@@ -0,0 +1,107 @@
+﻿using Android.Graphics.Drawables;
+using Microsoft.Maui.Controls;
+using Shipwreck.BootstrapControls;
+using Shipwreck.FontAwesomeControls;
+using static Android.Icu.Text.CaseMap;
+
+namespace Shipwreck.ViewModelUtils;
+
+public partial class CommandButton : ContentView
+{
+    public static readonly BindableProperty FontSizeProperty
+        = BindableProperty.Create(
+            nameof(FontSize), typeof(double), typeof(CommandButton),
+            defaultValue: new FontSizeConverter().ConvertFromInvariantString("Default"));
+
+    public static readonly BindableProperty ButtonPaddingProperty
+        = BindableProperty.Create(nameof(ButtonPadding), typeof(Thickness), typeof(CommandButton), defaultValue: new Thickness(8));
+
+    public CommandButton()
+    {
+        FontAwesome.Init();
+        BootstrapControlsModule.Init();
+        InitializeComponent();
+
+        OnButtonPaddingChanged();
+        OnFontSizeChanged();
+        OnFrameTextColorChanged();
+
+        frame.PropertyChanged += (s, e) =>
+        {
+            switch (e.PropertyName)
+            {
+                case nameof(frame.TextColor):
+                    OnFrameTextColorChanged();
+                    break;
+            }
+        };
+    }
+
+    [TypeConverter(typeof(FontSizeConverter))]
+    public double FontSize
+    {
+        get => (double)GetValue(FontSizeProperty);
+        set => SetValue(FontSizeProperty, value);
+    }
+
+    public Thickness ButtonPadding
+    {
+        get => (Thickness)GetValue(ButtonPaddingProperty);
+        set => SetValue(ButtonPaddingProperty, value);
+    }
+
+    protected override void OnPropertyChanged([CallerMemberName] string propertyName = null)
+    {
+        base.OnPropertyChanged(propertyName);
+
+        switch (propertyName)
+        {
+            case nameof(ButtonPadding):
+                OnButtonPaddingChanged();
+                break;
+
+            case nameof(FontSize):
+                OnFontSizeChanged();
+                break;
+        }
+    }
+
+    private void OnButtonPaddingChanged()
+    {
+        if (frame != null)
+        {
+            frame.Padding = ButtonPadding;
+        }
+    }
+
+    private void OnFontSizeChanged()
+    {
+        if (icon != null)
+        {
+            icon.FontSize = FontSize;
+        }
+
+        if (title != null)
+        {
+            title.FontSize = FontSize;
+        }
+
+        if (badgeCount != null)
+        {
+            badgeCount.FontSize = FontSize;
+        }
+    }
+
+    private void OnFrameTextColorChanged()
+    {
+        if (frame != null)
+        {
+            if (icon != null)
+            {
+                icon.TextColor = frame.TextColor;
+            }
+
+            title.TextColor = frame.TextColor;
+        }
+    }
+}
