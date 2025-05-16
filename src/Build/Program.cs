9cdaa3c8d7e484eadc91cd1bbda62f3737ba8606
pgrho <shipwreck@live.jp>
2025-05-16 16:38:29 +0900

    Avoid clean in default

--- src/Build/Program.cs
+++ src/Build/Program.cs
@@ -9,7 +9,7 @@ internal static class Program
 {
     private class Parameter
     {
-        public bool Clean { get; set; } = true;
+        public bool Clean { get; set; } = false;
         public bool Build { get; set; } = true;
         public string NugetSource { get; set; }
         public string NugetApiKey { get; set; }
