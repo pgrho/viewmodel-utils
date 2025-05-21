7afc168deef57e73a7830951507654b3fabe9d01
pgrho <shipwreck@live.jp>
2025-05-22 07:17:37 +0900

    Add DialogViewModel.IsSaving

--- src/Core/PresentationFramework/ViewModelUtils/DialogViewModel.cs
+++ src/Core/PresentationFramework/ViewModelUtils/DialogViewModel.cs
@@ -18,11 +18,25 @@ public abstract class DialogViewModel : WindowViewModel
     }
 
     private bool _IsAccepted;
+    private bool _IsSaving;
 
     public bool IsAccepted
     {
         get => _IsAccepted;
         protected set => SetProperty(ref _IsAccepted, value);
+    }
+
+    public bool IsSaving
+    {
+        get => _IsSaving;
+        protected set
+        {
+            if (SetProperty(ref _IsSaving, value))
+            {
+                _SaveCommand?.Invalidate();
+                _SaveAndCloseCommand?.Invalidate();
+            }
+        }
     }
 
     private CommandViewModelBase _SaveCommand;
@@ -35,20 +49,22 @@ public abstract class DialogViewModel : WindowViewModel
         => _SaveAndCloseCommand ??= GetSaveAndCloseCommandBuilder().Build();
 
     protected virtual CommandBuilderBase GetSaveCommandBuilder()
-        => new CommandBuilder(() => BeginSave(false))
+        => new CommandBuilder(_ => BeginSave(false))
         .SetTitle(SR.SaveTitle)
         .SetMnemonic(SR.SaveMnemonic)
         .SetDescription(SR.SaveDescription.EmptyToNull())
         .SetStyle(BorderStyle.OutlineSecondary)
-        .SetIsEnabled(() => CanAccept);
+        .SetIsEnabled(_ => CanAccept)
+        .SetIcon(c => c.IsExecuting || _IsSaving ? "fas fa-spinner fa-pulse" : "fas fa-save");
 
     protected virtual CommandBuilderBase GetSaveAndCloseCommandBuilder()
-        => new CommandBuilder(() => BeginSave(true))
+        => new CommandBuilder(_ => BeginSave(true))
         .SetTitle(SR.SaveAndCloseTitle)
         .SetMnemonic(SR.SaveAndCloseMnemonic)
         .SetDescription(SR.SaveAndCloseDescription.EmptyToNull())
         .SetStyle(BorderStyle.Primary)
-        .SetIsEnabled(() => CanAccept);
+        .SetIsEnabled(_ => CanAccept)
+        .SetIcon(c => c.IsExecuting || _IsSaving ? "fas fa-spinner fa-pulse" : "fas fa-save");
 
     protected abstract void BeginSave(bool shouldClose);
 }
