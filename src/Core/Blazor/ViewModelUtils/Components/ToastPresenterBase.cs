468c761b67725f479169589073c295e00b296bc8
pgrho <shipwreck@live.jp>
2025-07-04 15:01:47 +0900

    Make ToastPresenterBase default duration configurable

--- src/Core/Blazor/ViewModelUtils/Components/ToastPresenterBase.cs
+++ src/Core/Blazor/ViewModelUtils/Components/ToastPresenterBase.cs
@@ -13,6 +13,9 @@ public abstract class ToastPresenterBase : ListComponentBase<ToastData>
     [Parameter]
     public Action OnAdded { get; set; }
 
+    [Parameter]
+    public int Duration { get; set; } = 3000;
+
     public void Add(BorderStyle style, string message, string title, TimeSpan? duration = null)
     {
         var mc = Math.Max(0, MaximumCount - 1);
@@ -20,7 +23,9 @@ public abstract class ToastPresenterBase : ListComponentBase<ToastData>
         {
             Source.RemoveAt(Source.Count - 1);
         }
-        Source.Insert(0, new ToastData(style, title, message, duration ?? TimeSpan.FromSeconds(3)));
+        Source.Insert(
+            0,
+            new ToastData(style, title, message, duration ?? TimeSpan.FromMilliseconds(Duration)));
         OnAdded?.Invoke();
     }
 }
